2dabb2e27fcef5f7feb7c75abcfc91ea4698475d
Eric Rosenfeld <46537274+EricRosenfeld1@users.noreply.github.com>
2021-11-20 05:25:37 +0100

    initial commit

---
+++ CalypsoAPI.WinForms/MainForm.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace CalypsoAPI.WinForms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(51, 37);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 13);
+            this.lblStatus.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Status:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(17, 53);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(352, 163);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.WordWrap = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Calypso API";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 238);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblStatus);
+            this.Name = "MainForm";
+            this.Text = "Calypso API WinForms";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ CalypsoAPI.WinForms/MainForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CalypsoAPI.Core;
+using CalypsoAPI.Core.Events;
+
+namespace CalypsoAPI.WinForms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            var api = new Calypso(new ApiConfiguration(@"C:\Users\Public\Documents\Zeiss\CMMObserver"));
+            api.Initialize();
+
+            api.CMMStateChanged += Api_CMMStateChanged;
+            api.MeasurementInfoChanged += Api_MeasurementInfoChanged;
+        }
+
+        private void Api_MeasurementInfoChanged(object sender, MeasurementInfoEventargs e)
+        {
+            richTextBox1.Clear();
+            richTextBox1.AppendText($"Filename: {e.MeasurementInfo.MeasurementPlanFileName} \r\n");
+            richTextBox1.AppendText($"Plan ID : {e.MeasurementInfo.MeasurementPlanId} \r\n");
+            richTextBox1.AppendText($"Operator: {e.MeasurementInfo.OperatorId} \r\n");
+            richTextBox1.AppendText($"Part No : {e.MeasurementInfo.PartNumber} \r\n");
+            richTextBox1.AppendText($"HDR Path: {e.MeasurementInfo.HdrFilePath} \r\n");
+            richTextBox1.AppendText($"CHR Path: {e.MeasurementInfo.ChrFilePath} \r\n");
+            richTextBox1.AppendText($"FET Path: {e.MeasurementInfo.FetFilePath} \r\n");
+            richTextBox1.AppendText($"Control.: {e.MeasurementInfo.ControllerType} \r\n");
+            richTextBox1.AppendText($"Device  : {e.MeasurementInfo.DeviceGroup} \r\n");
+            richTextBox1.AppendText($"Firmware: {e.MeasurementInfo.FirmwareRevision} \r\n");
+        }
+
+        private void Api_CMMStateChanged(object sender, CMMStateChangedEventArgs e)
+        {
+            lblStatus.Text = e.Status.ToString();
+        }
+    }
+}
