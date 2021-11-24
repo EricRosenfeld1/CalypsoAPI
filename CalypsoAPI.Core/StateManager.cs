59deb176c6801223cd8c4c8997a7799ed0e59c84
Eric Rosenfeld <46537274+EricRosenfeld1@users.noreply.github.com>
2021-11-24 15:02:53 +0100

    Add files via upload

---
+++ CalypsoAPI.Core/StateManager.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using CalypsoAPI.Core.Models;
+
+namespace CalypsoAPI.Core
+{
+    public class StateManager
+    {
+        public async Task<ObserverFile> GetObserverFileAsync(string observerPath)
+        {
+            var path = Path.Combine(observerPath, "observerFile.txt");
+            if(File.Exists(path))
+            {
+                string data;
+                using (var reader = File.OpenText(path))
+                    data = await reader.ReadToEndAsync();
+
+                if (data.Length > 0)
+                    return ParseObserverFile(data);
+                else
+                    return null;
+            }
+
+            return null;
+        }
+
+        public async Task<CommandFile> GetCommandFileAsync(string observerPath)
+        {
+            var path = Path.Combine(observerPath, "observerCommandFile.txt");
+            if (File.Exists(path))
+            {
+                string data;
+                using (var reader = File.OpenText(path))
+                    data = await reader.ReadToEndAsync();
+
+                if (data.Length > 0)
+                    return ParseCommandFile(data);
+                else
+                    return null;
+            }
+            return null;
+        }
+
+        private ObserverFile ParseObserverFile(string data)
+        {
+            var observerFile = new ObserverFile();
+            List<string> lines = data.Split(new string[] { "\r\n" },StringSplitOptions.None).ToList();
+            foreach(var line in lines)
+            {
+                var tokens = line.Split(new string[] { "\t" }, StringSplitOptions.None);
+                if (tokens.Length == 2)
+                {
+                    try
+                    {
+                        observerFile.GetType().GetProperty(tokens[0].Trim()).SetValue(observerFile, tokens[1].Trim());
+                    } catch (Exception) { }
+                }
+            }
+
+            return observerFile;
+        }
+
+        public CommandFile ParseCommandFile(string data)
+        {
+            var commandFile = new CommandFile();
+            var tokens = data.Split(new string[] { "\t" }, StringSplitOptions.None);
+            if (tokens.Length > 0)
+                if(tokens[0].Trim() == "set_cnc_end" && tokens.Length == 5)
+                {
+                    commandFile.state = tokens[0].Trim();
+                    commandFile.toleranceState = tokens[1].Trim();
+                    commandFile.hdrPath = tokens[2].Trim();
+                    commandFile.fetPath = tokens[3].Trim();
+                    commandFile.chrPath = tokens[4].Trim();
+                } else if(tokens[0].Trim() == "set_cnc_start" && tokens.Length == 2)
+                {
+                    commandFile.planPath = tokens[1].Trim();
+                }
+                    
+            return commandFile;
+        }
+    }
+}
---
+++ CalypsoAPI.Core/StatusEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalypsoAPI.Core
+{
+    public enum Status
+    {
+        Running = 0,
+        Finished = 1,
+        Paused = 2,
+        Stopped = 3,
+        Exception = 4
+    }
+}
