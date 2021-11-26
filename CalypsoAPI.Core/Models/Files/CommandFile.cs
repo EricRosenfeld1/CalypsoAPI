82431069eba76b39cf61da468b326e84f5c0e9ca
Eric Rosenfeld <46537274+EricRosenfeld1@users.noreply.github.com>
2021-11-26 19:26:42 +0100

    Add files via upload

---
+++ CalypsoAPI.Core/Models/Files/CommandFile.cs
@@ -0,0 +1,12 @@
+﻿namespace CalypsoAPI.Core.Models.Files
+{
+    internal class CommandFile
+    {
+        internal string state { get; set; }
+        internal string toleranceState { get; set; }
+        internal string hdrPath { get; set; }
+        internal string chrPath { get; set; }
+        internal string fetPath { get; set; }
+        internal string planPath { get; set; }
+    }
+}
