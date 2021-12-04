584f524edf086f3b42e55f34a1c0a668115a5caa
Eric Rosenfeld <eric.rosenfeld@live.de>
2021-12-04 08:27:05 +0100

    remove swaggerUI

--- CalypsoAPI.WebApi/Startup.cs
+++ CalypsoAPI.WebApi/Startup.cs
@@ -19,23 +19,13 @@ namespace CalypsoAPI.WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CalypsoAPI.Rest", Version = "v1" });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseDeveloperExceptionPage();
-            app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CalypsoAPI.Rest v1"));
-
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
--- CalypsoAPI/AssemblyInfo.cs
+++ CalypsoAPI/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //      Buildnummer
 //      Revisionsnummer
 //
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
